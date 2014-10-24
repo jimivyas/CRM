e0f2a0875d8740638a0191695963b1cc978d3fd3
Jimi Vyas <jimipvyas@gmail.com>
2014-10-24 10:36:44 -0400

    fixed commit

---
+++ crm.rb
@@ -0,0 +1,20 @@
+
+def main_menu
+  puts "[1] Add a new contact"
+  puts "[2] Modify an existing contact"
+  puts "[3] Delete a contact"
+  puts "[4] Display all the contacts"
+  puts "[5] Display an attribute"
+  puts "[6] Exit"
+  puts "Enter a number: "
+  input = gets.chomp.to_i
+
+end
+
+def get_input
+end
+
+
+
+main_menu
+
